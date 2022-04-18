881d4ce1123af584afcb9e56d5741fb9930a3179
Tamer <tsalkin@gmail.com>
2022-04-18 11:04:30 -0500

    6. ServicesSection and FaqSection Style

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,6 @@
 import React from "react";
 import home1 from "../img/home1.png";
-//Styled
-import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 function AboutSection() {
   return (
@@ -32,37 +31,5 @@ function AboutSection() {
     </About>
   );
 }
-
-//Styled Components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover; //it will keep the aspect ratio
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
