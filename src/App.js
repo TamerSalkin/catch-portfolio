9b65f2cb462136cf90f2a81e44632acd575b64f0
Tamer <tsalkin@gmail.com>
2022-04-18 12:51:16 -0500

    7. React Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+//Router
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<AboutUs />}></Route>
+        <Route path="/work" element={<OurWork />}></Route>
+        <Route path="/contact" element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { About, Description, Image, Hide } from "../styles";
+import { About } from "../styles";
 import styled from "styled-components";
 
 function FaqSection() {
